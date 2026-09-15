032442ac1c34ec9703cdf35f8bd768d40e7fb335
Negzaxis-Harkwood <negzaxis@gmail.com>
2026-09-14 22:35:18 -0400

    G: added asteroid and asteroid_field.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     #intialize pygame
@@ -18,12 +20,22 @@ def main():
     # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # Add player to our groups
     Player.containers = (updatable, drawable)
 
+    # Add Asteroid to our groups
+    Asteroid.containers = (updatable, drawable, asteroids)
+
+    # Add AsteroidField to our groups
+    AsteroidField.containers = (updatable,)
+
     # instaniate player
     player = Player(x = SCREEN_WIDTH /2, y = SCREEN_HEIGHT / 2)
+
+    # create the asteroid field
+    asteroid_field = AsteroidField()
 
     # Game Loop
     while True:
